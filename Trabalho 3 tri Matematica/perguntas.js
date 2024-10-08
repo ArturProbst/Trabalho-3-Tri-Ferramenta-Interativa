77c83bb39668c70c73b2dcc8a1cf2b33ff6c4781
ArturProbst <144684741+ArturProbst@users.noreply.github.com>
2024-10-08 18:29:31 -0300

    Add files via upload

---
+++ Trabalho 3 tri Matematica/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao  (
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao (
+    'Lingua Inglesa',
+    'Como se diz oi em Inglês',
+    'Oi em Inglês é HI (RAI)'
+) 
